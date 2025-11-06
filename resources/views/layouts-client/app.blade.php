42eae7733cdd8de06e5128fec7d550354af325b2
biskecil <jjjuu2025@gmail.com>
2025-11-06 08:14:43 +0700

    if manajemen

--- resources/views/layouts-client/app.blade.php
+++ resources/views/layouts-client/app.blade.php
@@ -174,13 +174,15 @@
                                 Pricelist
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-secondary d-flex align-items-center {{ request()->is('user/*') ? 'active text-active fw-bold' : 'text-secondary fw-semibold' }} "
-                                href="/user/show">
-                                <i class="bi bi-receipt me-2"></i>
-                                Manajemen User
-                            </a>
-                        </li>
+                        @if (Auth::user() && Auth::user()->Role === 'administrator')
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary d-flex align-items-center {{ request()->is('user/*') ? 'active text-active fw-bold' : 'text-secondary fw-semibold' }}"
+                                    href="/user/show">
+                                    <i class="bi bi-receipt me-2"></i>
+                                    Manajemen User
+                                </a>
+                            </li>
+                        @endif
                     </ul>
                 </div>
             </nav>
